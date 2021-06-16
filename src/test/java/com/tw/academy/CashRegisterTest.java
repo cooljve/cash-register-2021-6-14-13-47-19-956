bb919523ccdb3ef5ecd6abbf06aa3155df7b311f
LIUJO5 <joi.liu@cargosmart.com>
2021-06-16 19:02:16 +0800

    implement own test double, and add unit test for CashRegister process

--- src/test/java/com/tw/academy/CashRegisterTest.java
+++ src/test/java/com/tw/academy/CashRegisterTest.java
@@ -2,15 +2,59 @@ package com.tw.academy;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CashRegisterTest {
-	@Test
-	void should_process_execute_printing() {
-		//given
-		CashRegister cashRegister = new CashRegister(new Printer());
-		Purchase purchase = new Purchase();
-		//when
-		cashRegister.process(purchase);
-		//then
-		//verify that cashRegister.process will trigger print
-	}
+
+    @Test
+    void should_invoke_print_when_invoke_process() {
+        //given
+        SpyPrinter printer = new SpyPrinter();
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = new Purchase();
+        //when
+        cashRegister.process(purchase);
+        //then
+        assertTrue(printer.invoke);
+    }
+
+    @Test
+    void should_print_purchase_asString_when_invoke_printer() {
+        //given
+        SpyPrinter printer = new SpyPrinter();
+        CashRegister cashRegister = new CashRegister(printer);
+        String printStr = "hello world";
+        StubPurchase purchase = new StubPurchase();
+        purchase.setResult(printStr);
+        //when
+        cashRegister.process(purchase);
+        //then
+        assertTrue(printer.invoke);
+        assertEquals(printStr, purchase.result);
+
+    }
+
+    private static class SpyPrinter extends Printer {
+        boolean invoke = false;
+
+        @Override
+        public void print(String content) {
+            invoke = true;
+        }
+
+    }
+
+    private static class StubPurchase extends Purchase {
+        String result;
+
+        void setResult(String result) {
+            this.result = result;
+        }
+
+        @Override
+        public String asString() {
+            return this.result;
+        }
+    }
 }
